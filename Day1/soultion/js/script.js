adc9c909cacabd59a2df18d0f5ffab193ffa5c0f
Amir <sagan.5050@gmail.com>
2024-09-03 12:43:47 +0300

    add soultion of first task

---
+++ Day1/soultion/js/script.js
@@ -0,0 +1,11 @@
+// TODO: first Task undifined mean that the variable is not defined yet or its not pinting to any value its like a box with no value inside it
+// null mean that the variable is defined but it has no value and (null) it self its a value
+
+var namee;
+
+console.log(typeof(namee)); // Output: undefined
+
+var age = null;
+
+console.log(age); // Output: null
+
